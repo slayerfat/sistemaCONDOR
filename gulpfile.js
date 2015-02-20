ec71c7ed3b77548d150fe45792486535b5b1f93f
slayerfat <slayerfat@gmail.com>
2015-02-20 15:31:55 -0430

    añadido bump (pensando si poner o no autotag)

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,43 @@
 var elixir = require('laravel-elixir');
 
+/**
+ * gulp bump
+ * 
+ * suponiendo que verion actual es 1.0.0
+ * 
+ * para prerelease: gulp bump-pre   | 1.0.0-0
+ * para patch:      gulp bump-patch | 1.0.1
+ * para minor:      gulp bump-minor | 1.1.0
+ * para major:      gulp bump-major | 2.0.0
+ */
+var gulp = require('gulp');
+var bump = require('gulp-bump');
+
+gulp.task('bump-patch', function(){
+  gulp.src(['./bower.json', 'package.json'])
+  .pipe(bump())
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump-minor', function(){
+  gulp.src(['./bower.json', 'package.json'])
+  .pipe(bump({type:'minor'}))
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump-major', function(){
+  gulp.src(['./bower.json', 'package.json'])
+  .pipe(bump({type:'major'}))
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump-pre', function(){
+  gulp.src(['./bower.json', 'package.json'])
+  .pipe(bump({type:'prerelease'}))
+  .pipe(gulp.dest('./'));
+});
+
+
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
