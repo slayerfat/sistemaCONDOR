b7d2bd94d686805c38f078a1a2b64ab6c541447e
slayerfat <slayerfat@gmail.com>
2015-02-19 23:28:42 -0430

    mejorada parcialmente la autenticacion de eventos

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -9,114 +9,123 @@ use Auth;
 
 class EventsController extends Controller {
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		$usuario = Auth::user();
-		$usuario->apartamentos;
-		foreach ($usuario->apartamentos()->get() as $apartamento) :
-			$edificios[] = $apartamento->edificio;
-		endforeach;
-		return view('events.index', compact('usuario', 'edificios'));
-	}
+  /**
+   * Display a listing of the resource.
+   *
+   * @return Response
+   */
+  public function index()
+  {
+    $usuario = Auth::user();
+    $usuario->apartamentos;
+    foreach ($usuario->apartamentos()->get() as $apartamento) :
+      $edificios[] = $apartamento->edificio;
+    endforeach;
+    return view('events.index', compact('usuario', 'edificios'));
+  }
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		$data = Auth::user()->perfiles;
-		if (sizeof($data) === 0): 
-			flash()->error('Error, Ud. no tiene permisos para esta accion.');
-			return redirect()->back();
-		endif;
-		$types = EventType::lists('description', 'id');
-		$evento = new Event;
-		return view('events.create', compact('types', 'evento'));
-	}
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return Response
+   */
+  public function create()
+  {
+    // toda esta mamarrachada tiene que ser limpiada
+    $data = Auth::user()->perfiles()->get()->toArray();
+    // todo esto es un asco
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store(EventRequest $request)
-	{
-		// se crea un nuevo objeto con los valores de request
-		$evento = new Event($request->all());
-		// se asignan el creado por y actualizado por
-		$evento->user_id    = Auth::user()->id;
-		$evento->created_by = Auth::user()->id;
-		$evento->updated_by = Auth::user()->id;
-		
-		$edificio = Auth::user()->edificios->first();
-		$edificio = Building::find($edificio->id);
-		$edificio->eventos()->save($evento);
-		// evento de exito
-		flash('Su Mensaje ha sido creado con exito.');
-		return redirect()->action('IndexController@index');
-	}
+    // se chequea que no este vacio
+    if (sizeof($data) === 0): 
+      flash()->error('Ud. no tiene permisos para esta accion.');
+      return redirect()->back();
+    // se chequea el tipo de perfil
+    elseif ($data[0]['description'] !== 'El Elegido' and $data[0]['description'] !== 'Administrador') :
+      flash()->error('Ud. no tiene permisos para esta accion.');
+      return redirect()->back();
+    endif;
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		$evento = Event::findOrFail($id);
-		return view('events.show', compact('evento'));
-	}
+    $types = EventType::lists('description', 'id');
+    $evento = new Event;
+    return view('events.create', compact('types', 'evento'));
+  }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		// se busca el evento solicitado o falla
-		$evento = Event::findOrFail($id);
-		// los tipos de mensajes
-		$types   = EventType::lists('description', 'id');
-		return view('events.edit', compact('evento', 'types'));
-	}
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @return Response
+   */
+  public function store(EventRequest $request)
+  {
+    // se crea un nuevo objeto con los valores de request
+    $evento = new Event($request->all());
+    // se asignan el creado por y actualizado por
+    $evento->user_id    = Auth::user()->id;
+    $evento->created_by = Auth::user()->id;
+    $evento->updated_by = Auth::user()->id;
+    
+    $edificio = Auth::user()->edificios->first();
+    $edificio = Building::find($edificio->id);
+    $edificio->eventos()->save($evento);
+    // evento de exito
+    flash('Su Mensaje ha sido creado con exito.');
+    return redirect()->action('IndexController@index');
+  }
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id, EventRequest $request)
-	{
-		// se busca el evento solicitado o falla
-		$evento = Event::findOrFail($id);
-		$evento->updated_by = Auth::user()->id;
-		// actualiza el evento
-		$evento->update($request->all());
-		// mensaje de exito
-		flash('El Evento ha sido actualizado con exito.');
-		return redirect()->action('IndexController@index');
-	}
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function show($id)
+  {
+    $evento = Event::findOrFail($id);
+    return view('events.show', compact('evento'));
+  }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function edit($id)
+  {
+    // se busca el evento solicitado o falla
+    $evento = Event::findOrFail($id);
+    // los tipos de mensajes
+    $types   = EventType::lists('description', 'id');
+    return view('events.edit', compact('evento', 'types'));
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function update($id, EventRequest $request)
+  {
+    // se busca el evento solicitado o falla
+    $evento = Event::findOrFail($id);
+    $evento->updated_by = Auth::user()->id;
+    // actualiza el evento
+    $evento->update($request->all());
+    // mensaje de exito
+    flash('El Evento ha sido actualizado con exito.');
+    return redirect()->action('IndexController@index');
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function destroy($id)
+  {
+    //
+  }
 
 }
