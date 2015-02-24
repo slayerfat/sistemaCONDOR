c583f3f9eaa6a39af5bd34286a2e30133464e017
slayerfat <slayerfat@gmail.com>
2015-02-24 14:47:05 -0430

    conserjeria

--- app/Http/Controllers/AssignApartmentsController.php
+++ app/Http/Controllers/AssignApartmentsController.php
@@ -9,94 +9,94 @@ use Illuminate\Http\Request;
 
 class AssignApartmentsController extends Controller {
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		// hacer resource o algo para validar como en eventscontroller
-		$edificios = Building::all();
-		$usuario = Auth::user()->apartamentos;
-		return view('assignApartments.index', compact('edificios', 'usuario'));
-	}
+  /**
+   * Display a listing of the resource.
+   *
+   * @return Response
+   */
+  public function index()
+  {
+    // hacer resource o algo para validar como en eventscontroller
+    $edificios = Building::all();
+    $usuario = Auth::user()->apartamentos;
+    return view('assignApartments.index', compact('edificios', 'usuario'));
+  }
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create($id)
-	{
-		$edificio = Building::findOrFail($id);
-		$apartamentos = Apartment::lists('number', 'id');
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return Response
+   */
+  public function create($id)
+  {
+    $edificio = Building::findOrFail($id);
+    $apartamentos = Apartment::lists('number', 'id');
 
-		$apartamentos = Apartment::listaHumana($apartamentos);
+    $apartamentos = Apartment::listaHumana($apartamentos);
 
-		return view('assignApartments.create', compact('edificio', 'apartamentos'));
-	}
+    return view('assignApartments.create', compact('edificio', 'apartamentos'));
+  }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store($id, Request $request)
-	{
-		// validacion de campos
-		$this->validate($request, [
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @return Response
+   */
+  public function store($id, Request $request)
+  {
+    // validacion de campos
+    $this->validate($request, [
       'apartment_id' => 'required|integer'
     ]);
-		$edificio = Building::findOrFail($id);
+    $edificio = Building::findOrFail($id);
 
-		Auth::user()->apartamentos()->attach($request->input('apartment_id'));
-		
-		return redirect()->action('IndexController@index');
-	}
+    Auth::user()->apartamentos()->attach($request->input('apartment_id'));
+    
+    return redirect()->action('IndexController@index');
+  }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function show($id)
+  {
+    //
+  }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function edit($id)
+  {
+    //
+  }
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function update($id)
+  {
+    //
+  }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function destroy($id)
+  {
+    //
+  }
 
 }
--- app/Http/Requests/ItemsRequest.php
+++ app/Http/Requests/ItemsRequest.php
@@ -4,28 +4,28 @@ use App\Http\Requests\Request;
 
 class ItemsRequest extends Request {
 
-	/**
-	 * Determine if the user is authorized to make this request.
-	 *
-	 * @return bool
-	 */
-	public function authorize()
-	{
-		return true;
-	}
+  /**
+   * Determine if the user is authorized to make this request.
+   *
+   * @return bool
+   */
+  public function authorize()
+  {
+    return true;
+  }
 
-	/**
-	 * Get the validation rules that apply to the request.
-	 *
-	 * @return array
-	 */
-	public function rules()
-	{
-		return [
-			'building_id' => 'required|integer',
-			'description' => 'required|string|min:3|max:255',
-			'total'       => 'integer|max:9999999999'
-		];
-	}
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array
+   */
+  public function rules()
+  {
+    return [
+      'building_id' => 'required|integer',
+      'description' => 'required|string|min:3|max:255',
+      'total'       => 'integer|max:9999999999'
+    ];
+  }
 
 }
