d91dff5e54f596c9b9e493f51cf837237eee4ce4
slayerfat <slayerfat@gmail.com>
2015-02-25 17:14:46 -0430

    conserjeria

--- database/migrations/2015_02_25_212553_create_movement_types_table.php
+++ database/migrations/2015_02_25_212553_create_movement_types_table.php
@@ -5,34 +5,34 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateMovementTypesTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('movement_types', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->string('description');
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('movement_types', function(Blueprint $table)
+    {
+      $table->increments('id');
+      $table->string('description');
       $table->timestamps();
       $table->integer('created_by')->unsigned();
       $table->foreign('created_by')->references('id')->on('users');
       $table->integer('updated_by')->unsigned();
       $table->foreign('updated_by')->references('id')->on('users');
       $table->softDeletes();
-		});
-	}
+    });
+  }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('movement_types');
-	}
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::drop('movement_types');
+  }
 
 }
--- database/migrations/2015_02_25_213311_add_foreign_key_to_movements_table.php
+++ database/migrations/2015_02_25_213311_add_foreign_key_to_movements_table.php
@@ -5,32 +5,32 @@ use Illuminate\Database\Migrations\Migration;
 
 class AddForeignKeyToMovementsTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('movements', function(Blueprint $table)
-		{
-			$table->integer('movement_type_id')->unsigned();
-			$table->foreign('movement_type_id')->references('id')->on('movement_types');
-		});
-	}
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('movements', function(Blueprint $table)
+    {
+      $table->integer('movement_type_id')->unsigned();
+      $table->foreign('movement_type_id')->references('id')->on('movement_types');
+    });
+  }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::table('movements', function(Blueprint $table)
-		{
-			$table->dropForeign('movements_movement_type_id_foreign');
-			$table->dropColumn('movement_type_id');
-		});
-	}
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::table('movements', function(Blueprint $table)
+    {
+      $table->dropForeign('movements_movement_type_id_foreign');
+      $table->dropColumn('movement_type_id');
+    });
+  }
 
 }
