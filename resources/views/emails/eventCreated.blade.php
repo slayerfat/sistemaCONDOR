a075f8c8cfe1e34b50ded9b80361a35747740436
slayerfat <slayerfat@gmail.com>
2015-02-28 18:56:16 -0430

    mejorado estilo para gmail

--- resources/views/emails/eventCreated.blade.php
+++ resources/views/emails/eventCreated.blade.php
@@ -1,28 +1,38 @@
-<!DOCTYPE html>
-<html>
-<body>
-  <h1>Nuevo Evento Creado!</h1>
-  <section>
+@extends('emails.structure')
+
+@section('content')
+  <section style="color: #333;font-family: sans-serif;">
+    <h1>Nuevo Evento Creado!</h1>
     <h2>
       El Usuario
-      {{ $usuario->first_name }}
-      {{ $usuario->first_surname }}
+      {!! link_to_action('UsersController@show',
+          $usuario->first_name.', '.$usuario->first_surname,
+          $usuario->id,
+          ['style' => 'font-family: sans-serif;color: #337ab7;text-decoration: none !important;']
+      ) !!}
     </h2>
     <p>
       Ha creado un nuevo evento en el sistemaCONDOR:
     </p>
   </section>
-  <h3>{{ $evento->title }}</h3>
-  <p>
-    {{ $evento->body }}
-  </p>
-  <p>
-    <strong>
-      {{ $evento->tipo->description }}
-    </strong>
-  </p>
-  <p>
-    {{ $evento->created_at }}
-  </p>
-</body>
-</html>
+  <section style="color: #333;font-family: sans-serif;">
+    <h3>
+      {!! link_to_action('EventsController@show',
+          $evento->title,
+          $evento->id,
+          ['style' => 'font-family: sans-serif;color: #337ab7;text-decoration: none !important;']
+      ) !!}
+    </h3>
+    <p id="body" style="text-align: justify;">
+      {{ $evento->body }}
+    </p>
+    <p>
+      <strong>
+        {{ $evento->tipo->description }}
+      </strong>
+    </p>
+    <p>
+      {{ $evento->created_at }}
+    </p>
+  </section>
+@stop
