44d96627e10ad81f14893feaf41e81fb4970f640
slayerfat <slayerfat@gmail.com>
2015-02-28 21:56:03 -0430

    traducido formularios de login y registrar

--- resources/views/errors/lista.blade.php
+++ resources/views/errors/lista.blade.php
@@ -1,5 +1,11 @@
 @if ($errors->any())
   <div class="alert alert-danger">
+    <div id="error-message">
+      <strong>Oops!</strong>
+      <span>
+        Parece que hay problemas con los datos suministrados.
+      </span>
+    </div>
     <ul>
       @foreach ($errors->all() as $error)
         <li>{{ $error }}</li>
