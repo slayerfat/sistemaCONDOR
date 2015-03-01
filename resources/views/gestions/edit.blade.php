454f37ecb569dd7fbea17c3127bcd9d665cba3cd
slayerfat <slayerfat@gmail.com>
2015-03-01 12:07:19 -0430

    ajustado gestion multifamiliar

--- resources/views/gestions/edit.blade.php
+++ resources/views/gestions/edit.blade.php
@@ -6,23 +6,28 @@
 
 @section('contenido')
   <div class="container">
-    <h1>
-      Actualizar Miembro de la Gestion Multifamiliar del Edificio
-      {!! link_to_action('BuildingsController@show',
-        $edificio->name,
-        $edificio->id
-      ) !!}
-    </h1>
-
-    @include('errors.lista')
-    
-    <hr/>
-
-    {!! Form::model($edificio, [
-          'method' => 'PATCH', 
-          'action' => ['GestionsController@update', $edificio->id]
-        ]) !!}
-      @include('gestions._form', ['textoBotonSubmit' => 'Actualizar nuevo Miembro'])
-    {!! Form::close() !!}    
+    <div class="row">
+      <div class="col-lg-10 col-lg-offset-1">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            Actualizar Miembro de la Gestion Multifamiliar del Edificio
+            {!! link_to_action('BuildingsController@show',
+              $edificio->name,
+              $edificio->id
+            ) !!}
+          </div>
+          <div class="panel-body">
+            @include('errors.lista')
+            {!! Form::model($edificio, [
+                  'method' => 'PATCH',
+                  'action' => ['GestionsController@update', $edificio->id],
+                  'class'  => 'form-horizontal',
+                ]) !!}
+              @include('gestions._form', ['textoBotonSubmit' => 'Actualizar nuevo Miembro'])
+            {!! Form::close() !!}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 @stop
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -7,14 +7,15 @@
 @section('contenido')
   <div class="container">
     <div class="row">
-    <div class="col-lg-10 col-lg-offset-1">
-      <div class="panel panel-default">
-        <div class="panel-heading">Crea un nuevo Usuario en el sistemaCONDOR</div>
-        <div class="panel-body">
-          @include('errors.lista')
-          {!! Form::open(['action' => 'UsersController@store', 'class' => 'form-horizontal']) !!}
-            @include('users._form', ['textoBotonSubmit' => 'Añadir nuevo Usario'])
-          {!! Form::close() !!}
+      <div class="col-lg-10 col-lg-offset-1">
+        <div class="panel panel-default">
+          <div class="panel-heading">Crea un nuevo Usuario en el sistemaCONDOR</div>
+          <div class="panel-body">
+            @include('errors.lista')
+            {!! Form::open(['action' => 'UsersController@store', 'class' => 'form-horizontal']) !!}
+              @include('users._form', ['textoBotonSubmit' => 'Añadir nuevo Usario'])
+            {!! Form::close() !!}
+          </div>
         </div>
       </div>
     </div>
