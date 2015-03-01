977c71275782fc85c5c3d20c1481c3810c1c90e9
slayerfat <slayerfat@gmail.com>
2015-03-01 12:24:17 -0430

    ajustado edifico y asignar apartamento

--- resources/views/assignApartments/create.blade.php
+++ resources/views/assignApartments/create.blade.php
@@ -6,12 +6,22 @@
 
 @section('contenido')
   <div class="container">
-    @include('errors.lista')
-    {!! Form::model($edificio, [
-          'method' => 'POST', 
-          'action' => ['AssignApartmentsController@store', $edificio->id]
-        ]) !!}
-      @include('assignApartments._form', ['textoBotonSubmit' => 'Pedir Solicitud'])
-    {!! Form::close() !!}
+    <div class="row">
+      <div class="col-lg-10 col-lg-offset-1">
+        <div class="panel panel-default">
+          <div class="panel-heading">Crea un nuevo Usuario en el sistemaCONDOR</div>
+          <div class="panel-body">
+            @include('errors.lista')
+            {!! Form::model($edificio, [
+                  'method' => 'POST',
+                  'action' => ['AssignApartmentsController@store', $edificio->id],
+                  'class'  => 'form-horizontal'
+                ]) !!}
+              @include('assignApartments._form', ['textoBotonSubmit' => 'Pedir Solicitud'])
+            {!! Form::close() !!}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 @stop
